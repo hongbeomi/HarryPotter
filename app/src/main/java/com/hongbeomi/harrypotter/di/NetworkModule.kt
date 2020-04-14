f2b61002d2197755201a1fa92936bb2e9992f5b8
hongbeomi <dks3963@naver.com>
2020-04-14 16:39:21 +0900

    라이센스 추가

--- app/src/main/java/com/hongbeomi/harrypotter/di/NetworkModule.kt
+++ app/src/main/java/com/hongbeomi/harrypotter/di/NetworkModule.kt
@@ -1,20 +1,29 @@
 package com.hongbeomi.harrypotter.di
 
-import com.google.gson.GsonBuilder
-import com.google.gson.JsonSyntaxException
-import com.google.gson.TypeAdapter
-import com.google.gson.stream.JsonReader
-import com.google.gson.stream.JsonToken
-import com.google.gson.stream.JsonWriter
+/**
+ * Copyright 2020 Hongbeom Ahn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **/
+
 import com.hongbeomi.harrypotter.data.service.HarryPotterService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.io.IOException
 import java.util.concurrent.TimeUnit
-
 
 private const val CONNECT_TIMEOUT = 10L
 private const val WRITE_TIMEOUT = 1L
--- app/src/main/java/com/hongbeomi/harrypotter/ui/SpaceItemDecoration.kt
+++ app/src/main/java/com/hongbeomi/harrypotter/ui/SpaceItemDecoration.kt
@@ -1,10 +1,26 @@
 package com.hongbeomi.harrypotter.ui
 
+/**
+ * Copyright 2020 Hongbeom Ahn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **/
+
 import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
-
 
 class SpaceItemDecoration(private val space: Int, private val mNumCol: Int) : ItemDecoration() {
 
