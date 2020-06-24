333043005590a56233128872d6564fe1ba312c07
hongbeomi <dks3963@naver.com>
2020-06-24 12:53:26 +0900

    dagger-hilt gradle setting

--- app/src/main/java/com/hongbeomi/harrypotter/HarryPotterApp.kt
+++ app/src/main/java/com/hongbeomi/harrypotter/HarryPotterApp.kt
@@ -18,24 +18,11 @@ package com.hongbeomi.harrypotter
  **/
 
 import android.app.Application
-import com.hongbeomi.harrypotter.di.dataSourceModule
-import com.hongbeomi.harrypotter.di.networkModule
-import com.hongbeomi.harrypotter.di.repositoryModule
-import com.hongbeomi.harrypotter.di.viewModelModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
 
 class HarryPotterApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin {
-            androidContext(this@HarryPotterApp)
-            modules(dataSourceModule)
-            modules(repositoryModule)
-            modules(viewModelModule)
-            modules(networkModule)
-        }
     }
 
 }
